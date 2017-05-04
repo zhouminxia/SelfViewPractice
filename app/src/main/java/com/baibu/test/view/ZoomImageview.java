3d0a5ca712c152c6ee4581523a761b22891d7413
zhouminxia <469358386@qq.com>
2017-05-04 17:53:35 +0800

    第一次缩放，不管原图片大小是多少，都弄成正中间的。但是无法进行第二次缩放了。====

--- app/src/main/java/com/baibu/test/view/ZoomImageview.java
+++ app/src/main/java/com/baibu/test/view/ZoomImageview.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.graphics.Matrix;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 
@@ -14,13 +17,16 @@ import android.widget.ImageView;
  * 1.图片居中缩放；布局里面控件设置为全Matchparent，图片src自动居中缩放。
  * 2.多指缩放。
  */
-public class ZoomImageview extends ImageView implements ViewTreeObserver.OnGlobalLayoutListener {
+public class ZoomImageview extends ImageView implements ViewTreeObserver.OnGlobalLayoutListener, ScaleGestureDetector.OnScaleGestureListener, View.OnTouchListener {
 
     private float mIniScale = 1;
     private float mMaxScale;
     private float mMidScale;//双击时的缩放
     private boolean firstInit = false;
-    private Matrix matrix;
+    private Matrix mMatrix;
+    private Context mContext;
+    private ScaleGestureDetector mScaleGestureDetector;
+    private float mScale;//第一次缩放的
 
     public ZoomImageview(Context context) {
         this(context, null);
@@ -32,12 +38,16 @@ public class ZoomImageview extends ImageView implements ViewTreeObserver.OnGloba
 
     public ZoomImageview(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(context);
     }
 
-    private void init() {
-        setScaleType(ScaleType.MATRIX);
-        matrix = new Matrix();
+    private void init(Context context) {
+        mContext = context;
+        mMatrix = new Matrix();
+//        setScaleType(ScaleType.MATRIX);//不知道为什么设置这句话后，图片就变小了
+
+        mScaleGestureDetector = new ScaleGestureDetector(mContext, this);
+        setOnTouchListener(this);
     }
 
     @Override
@@ -68,7 +78,7 @@ public class ZoomImageview extends ImageView implements ViewTreeObserver.OnGloba
             int intrinsicWidth = drawable.getIntrinsicWidth();
             int intrinsicHeight = drawable.getIntrinsicHeight();
 
-            float mScale = 1;
+            mScale = 1;
             //intrinsicWidth大，intrinsicHeight大
             if (intrinsicHeight > height && intrinsicWidth > width) {
                 mScale = Math.min(intrinsicHeight * 1.0f / height, intrinsicWidth * 1.0f / width);
@@ -97,9 +107,60 @@ public class ZoomImageview extends ImageView implements ViewTreeObserver.OnGloba
 
             //利用matrix移动和缩放
             //先平移，再缩放
-            matrix.postTranslate(dx, dy);
-            matrix.postScale(mIniScale, mIniScale, getWidth() / 2, getHeight() / 2);
-            setImageMatrix(matrix);
+            mMatrix.postTranslate(dx, dy);
+            mMatrix.postScale(mIniScale, mIniScale, getWidth() / 2, getHeight() / 2);
+            setImageMatrix(mMatrix);
         }
+    }
+
+    /**
+     * 当前matrix缩放比例
+     *
+     * @return
+     */
+    private float getCurrentScale() {
+        float[] v = new float[9];
+        mMatrix.getValues(v);
+        return v[Matrix.MSCALE_X];
+    }
+
+    @Override
+    public boolean onScale(ScaleGestureDetector detector) {
+        if (getDrawable() == null) return true;
+
+//        float scaleFactor = detector.getScaleFactor();
+        float scaleFactor = mScale;
+        float currentScale = getCurrentScale();
+
+        if ((scaleFactor > 1.0f && scaleFactor * currentScale < mMaxScale) || scaleFactor < 1.0f && scaleFactor * currentScale > mIniScale) {
+
+            if (scaleFactor * currentScale < mIniScale) {
+                scaleFactor = mIniScale / currentScale;
+            }
+            if (scaleFactor * currentScale > mMaxScale) {
+                scaleFactor = mMaxScale / currentScale;
+            }
+        }
+
+        mMatrix.postScale(scaleFactor, scaleFactor, getWidth() / 2, getHeight() / 2);
+        setImageMatrix(mMatrix);
+
+        return true;
+    }
+
+    @Override
+    public boolean onScaleBegin(ScaleGestureDetector detector) {
+        return true;
+    }
+
+    @Override
+    public void onScaleEnd(ScaleGestureDetector detector) {
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        mScaleGestureDetector.onTouchEvent(event);
+        return true;
     }
 }
