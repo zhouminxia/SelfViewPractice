d1f8f7a1cb3de1a52f50166e712da5cd43af75eb
zhouminxia <469358386@qq.com>
2017-05-05 10:14:28 +0800

    解决昨天遗留问题；

--- app/src/main/java/com/baibu/test/view/ZoomImageview.java
+++ app/src/main/java/com/baibu/test/view/ZoomImageview.java
@@ -9,6 +9,7 @@ import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by minna_Zhou on 2017/5/4.
@@ -27,6 +28,8 @@ public class ZoomImageview extends ImageView implements ViewTreeObserver.OnGloba
     private Context mContext;
     private ScaleGestureDetector mScaleGestureDetector;
     private float mScale;//第一次缩放的
+    private int mViewWidth;
+    private int mViewHeight;
 
     public ZoomImageview(Context context) {
         this(context, null);
@@ -44,8 +47,7 @@ public class ZoomImageview extends ImageView implements ViewTreeObserver.OnGloba
     private void init(Context context) {
         mContext = context;
         mMatrix = new Matrix();
-//        setScaleType(ScaleType.MATRIX);//不知道为什么设置这句话后，图片就变小了
-
+        setScaleType(ScaleType.MATRIX);//不知道为什么设置这句话后，图片就变小了
         mScaleGestureDetector = new ScaleGestureDetector(mContext, this);
         setOnTouchListener(this);
     }
@@ -64,12 +66,11 @@ public class ZoomImageview extends ImageView implements ViewTreeObserver.OnGloba
 
     @Override
     public void onGlobalLayout() {
+        //整个控件的宽高
+        mViewWidth = getWidth();
+        mViewHeight = getHeight();
         if (!firstInit) {
             firstInit = true;
-
-            //整个控件的宽高
-            int width = getWidth();
-            int height = getHeight();
 
             Drawable drawable = getDrawable();
             if (drawable == null) return;
@@ -80,35 +81,34 @@ public class ZoomImageview extends ImageView implements ViewTreeObserver.OnGloba
 
             mScale = 1;
             //intrinsicWidth大，intrinsicHeight大
-            if (intrinsicHeight > height && intrinsicWidth > width) {
-                mScale = Math.min(intrinsicHeight * 1.0f / height, intrinsicWidth * 1.0f / width);
+            if (intrinsicHeight > mViewHeight && intrinsicWidth > mViewWidth) {
+                mScale = Math.min(intrinsicHeight * 1.0f / mViewHeight, intrinsicWidth * 1.0f / mViewWidth);
             }
             //intrinsicWidth小，intrinsicHeight小
-            if (intrinsicHeight < height && intrinsicWidth < width) {
-                mScale = Math.min(intrinsicHeight * 1.0f / height, intrinsicWidth * 1.0f / width);
+            if (intrinsicHeight < mViewHeight && intrinsicWidth < mViewWidth) {
+                mScale = Math.min(mViewHeight * 1.0f / intrinsicHeight, mViewWidth * 1.0f / intrinsicWidth);
             }
             //intrinsicWidth大，intrinsicHeight小
-            if (intrinsicHeight > height && intrinsicWidth < width) {
-                mScale = intrinsicHeight * 1.0f / height;
+            if (intrinsicHeight > mViewHeight && intrinsicWidth < mViewWidth) {
+                mScale = intrinsicHeight * 1.0f / mViewHeight;
             }
             //intrinsicWidth小，intrinsicHeight大
-            if (intrinsicHeight < height && intrinsicWidth > width) {
-                mScale = intrinsicWidth * 1.0f / width;
+            if (intrinsicHeight < mViewHeight && intrinsicWidth > mViewWidth) {
+                mScale = intrinsicWidth * 1.0f / mViewWidth;
             }
 
             mIniScale = mScale;
             mMaxScale = mIniScale * 4;
             mMidScale = mIniScale * 2;
 
-
             //把图片移动到屏幕中心,移动的dx dy
-            int dx = getWidth() / 2 - intrinsicWidth / 2;
-            int dy = getHeight() / 2 - intrinsicHeight / 2;
+            int dx = mViewWidth / 2 - intrinsicWidth / 2;
+            int dy = mViewHeight / 2 - intrinsicHeight / 2;
 
             //利用matrix移动和缩放
             //先平移，再缩放
             mMatrix.postTranslate(dx, dy);
-            mMatrix.postScale(mIniScale, mIniScale, getWidth() / 2, getHeight() / 2);
+            mMatrix.postScale(mIniScale, mIniScale, mViewWidth / 2, mViewHeight / 2);
             setImageMatrix(mMatrix);
         }
     }
@@ -122,6 +122,7 @@ public class ZoomImageview extends ImageView implements ViewTreeObserver.OnGloba
         float[] v = new float[9];
         mMatrix.getValues(v);
         return v[Matrix.MSCALE_X];
+
     }
 
     @Override
@@ -129,10 +130,9 @@ public class ZoomImageview extends ImageView implements ViewTreeObserver.OnGloba
         if (getDrawable() == null) return true;
 
         float scaleFactor = detector.getScaleFactor();
+        Toast.makeText(mContext, "onScale,scaleFactor=" + scaleFactor, Toast.LENGTH_LONG).show();
         float currentScale = getCurrentScale();
-//        float currentScale = mScale;
-        System.out.println("--scaleFactor * currentScale=" + scaleFactor * currentScale+"<scaleFactor="+scaleFactor+",mMaxScale="+mMaxScale+",mIniScale="+mIniScale);
-        if ((scaleFactor > 1.0f && scaleFactor * currentScale < mMaxScale) || scaleFactor < 1.0f && scaleFactor * currentScale > mIniScale) {
+        if ((scaleFactor > 1.0f && currentScale < mMaxScale) || scaleFactor < 1.0f && currentScale > mIniScale) {
 
             if (scaleFactor * currentScale < mIniScale) {
                 scaleFactor = mIniScale / currentScale;
@@ -140,9 +140,7 @@ public class ZoomImageview extends ImageView implements ViewTreeObserver.OnGloba
             if (scaleFactor * currentScale > mMaxScale) {
                 scaleFactor = mMaxScale / currentScale;
             }
-
-            System.out.println("--sc2scaleFactor="+scaleFactor);
-            mMatrix.postScale(scaleFactor, scaleFactor, getWidth() / 2, getHeight() / 2);
+            mMatrix.postScale(scaleFactor, scaleFactor, mViewWidth / 2, mViewHeight / 2);
             setImageMatrix(mMatrix);
         }
         return true;
