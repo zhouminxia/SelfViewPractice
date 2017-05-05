27092fbfb74ae8810467c496dea0b2de3048674c
zhouminxia <469358386@qq.com>
2017-05-05 15:45:30 +0800

    自由缩放时，周边留白的解决；

--- app/src/main/java/com/baibu/test/view/ZoomImageview.java
+++ app/src/main/java/com/baibu/test/view/ZoomImageview.java
@@ -2,6 +2,7 @@ package com.baibu.test.view;
 
 import android.content.Context;
 import android.graphics.Matrix;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -14,7 +15,7 @@ import android.widget.Toast;
 /**
  * Created by minna_Zhou on 2017/5/4.
  * 可以自动缩放的imageview
- * <p/>
+ * <p>
  * 1.图片居中缩放；布局里面控件设置为全Matchparent，图片src自动居中缩放。
  * 2.多指缩放。
  */
@@ -73,7 +74,7 @@ public class ZoomImageview extends ImageView implements ViewTreeObserver.OnGloba
             firstInit = true;
 
             Drawable drawable = getDrawable();
-            if (drawable == null) return;
+            if (isDrawableNull()) return;
 
             //图片的宽高
             int intrinsicWidth = drawable.getIntrinsicWidth();
@@ -130,7 +131,7 @@ public class ZoomImageview extends ImageView implements ViewTreeObserver.OnGloba
 
     @Override
     public boolean onScale(ScaleGestureDetector detector) {
-        if (getDrawable() == null) return true;
+        if (isDrawableNull()) return true;
 
         float scaleFactor = detector.getScaleFactor();
         Toast.makeText(mContext, "onScale,scaleFactor=" + scaleFactor, Toast.LENGTH_LONG).show();
@@ -143,11 +144,99 @@ public class ZoomImageview extends ImageView implements ViewTreeObserver.OnGloba
             if (scaleFactor * currentScale > mMaxScale) {
                 scaleFactor = mMaxScale / currentScale;
             }
+
+            //永远以屏幕中心进行缩放
 //            mMatrix.postScale(scaleFactor, scaleFactor, mViewWidth / 2, mViewHeight / 2);
+
+            //以触摸点来进行缩放，但是有问题：会留白
             mMatrix.postScale(scaleFactor, scaleFactor, detector.getFocusX(), detector.getFocusY());
+
+            //时时来修正图片，设置图片偏移量，不要留白
+            fixBorderBlank();
+
             setImageMatrix(mMatrix);
         }
         return true;
+    }
+
+    /**
+     * 修正周围的留白部分，设置图片偏移量
+     * 思路：分别判断水平和垂直方向的留白部分，进行修正
+     */
+    private void fixBorderBlank() {
+
+
+        if (isDrawableNull()) {
+            return;
+        }
+        RectF rectF = getMatrxRectF();
+        //缩放后的图片的宽高
+        float picWidth = rectF.width();
+        float picHeight = rectF.height();
+        float deltaX = 0;//X轴的偏移量
+        float deltaY = 0;//Y轴的偏移量
+
+        //水平方向的
+        if (picWidth >= mViewWidth) {//大于宽度
+            //左边留白
+            if (rectF.left > 0) {
+                deltaX = -rectF.left;
+            }
+            //右边留白
+            if (mViewWidth - rectF.right > 0) {
+                deltaX = mViewWidth - rectF.right;
+            }
+        } else {//小于宽度
+            //居中
+            deltaX = mViewWidth / 2 - (rectF.right - picWidth / 2);
+        }
+
+        //垂直方向的
+        if (picHeight >= mViewHeight) {
+            //上面留白
+            if (rectF.top > 0) {
+                deltaY = -rectF.top;
+            }
+            //下面留白
+            if (mViewHeight - rectF.bottom > 0) {
+                deltaY = mViewHeight - rectF.bottom;
+            }
+        } else {
+            deltaY = mViewHeight / 2 - (rectF.bottom - picHeight / 2);
+        }
+
+        mMatrix.postTranslate(deltaX, deltaY);
+
+    }
+
+    /**
+     * 图片是否为空
+     * true 没有设置图片
+     *
+     * @return
+     */
+    private boolean isDrawableNull() {
+        return getDrawable() == null ? true : false;
+    }
+
+    /**
+     * 获得图片缩小/放大后的rectf，left top right bottom
+     *
+     * @return
+     */
+    private RectF getMatrxRectF() {
+        RectF rectF = new RectF();
+        Drawable d = getDrawable();
+
+        rectF.set(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
+        Matrix matrix = mMatrix;
+
+        /**测量rect并将测量结果放入rect中，
+         * Apply this matrix to the rectangle, and write the transformed rectangle
+         * back into it. This is accomplished by transforming the 4 corners of rect,
+         * and then setting it to the bounds of those points*/
+        matrix.mapRect(rectF);
+        return rectF;
     }
 
     @Override
